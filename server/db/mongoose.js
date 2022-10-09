b6f310057ef7d07204de1a37e23a201028f53a24
ashley <ashleylu2008@gmail.com>
2022-10-09 14:44:27 -0400

    Create module that will hold DB connection

---
+++ server/db/mongoose.js
@@ -0,0 +1,11 @@
+/* This module will hold our connection to our mongo server through the Mongoose API.
+We will access the connection in our express server. */
+const mongoose = require('mongoose');
+
+/* Connnect to our database */
+// TODO
+
+// Export the active connection.
+module.exports = {
+    mongoose
+};
