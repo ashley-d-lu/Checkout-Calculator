2d3bbf27c87a4c99c4d8a3093a39b35f6f33e101
ashley <ashleylu2008@gmail.com>
2022-10-10 18:04:18 -0400

    Test API call from client to express server. Add node engine for deployed Heroku app.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 
+const testApiCall = async () => {
+  const dev = process.env.NODE_ENV !== 'production';
+  const port = process.env.PORT || 5000;
+  const server = dev ? 'http://localhost:' + port : 'https://csc301-a2-pair-36.herokuapp.com';
+
+  let res = await fetch(server + "/api/province/taxes")
+  console.log(res);
+  console.log(res.json());
+
+  res = await fetch(server + "/api/cart")
+  console.log(res);
+  console.log(res.json());
+}
+
 function MyApp({ Component, pageProps }) {
+
+  testApiCall()
+
   return <Component {...pageProps} />
 }
 
