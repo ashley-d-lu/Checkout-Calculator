1276c4e8ada234ac01b9b26dbad83aabf5afd19c
allenchazhoor <allenchazhoor@gmail.com>
2022-10-14 00:59:10 -0400

    Add menu items with emojis

--- client/menu.js
+++ client/menu.js
@@ -3,17 +3,32 @@
  */
 export const menu = [
     {
-        name:"apple",
+        name:"Apple 🍎",
         price: 1.00,
         quantity: 0
     },
     {
-        name:"orange",
+        name:"Orange 🍊",
         price: 1.50,
         quantity:0
     },
     {
-        name:"pear",
+        name:"Banana 🍌",
+        price: 3.67,
+        quantity:0
+    },
+    {
+        name:"Mango 🥭",
+        price: 0.67,
+        quantity:0
+    },
+    {
+        name:"Grapes 🍇",
+        price: 1.34,
+        quantity:0
+    },
+    {
+        name:"Pear 🍐",
         price: 8.75,
         quantity:0
     }
