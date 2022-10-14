b531fd8fd920e2141989669ce89dbdd34f542308
ashley <ashleylu2008@gmail.com>
2022-10-14 19:02:08 -0400

    Add console.log to demo auto deploy

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
+  console.log('demo auto deploy!')
   return <Component {...pageProps} />
 }
 
